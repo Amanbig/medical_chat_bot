3d5594ba5629767fe42725e3e9e6228464984748
aman <amanpreetsinghjhiwant@outlook.com>
2024-11-02 20:23:41 +0530

    made static builds in next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+// // next.config.js
+// // next.config.mjs
+export default {
+    output: 'export',         // Enable static export mode
+  };
+  
