b9069622ec54a0425b536ada62969fbe34794276
Amanpreet Singh <amanpreetsinghjhiwant@outlook.com>
2024-11-30 11:44:39 +0530

    changed request mode

--- src/components/parts/FeaturesList.jsx
+++ src/components/parts/FeaturesList.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { ArrowRight } from "lucide-react";
-import { URL } from "../../../urls";
+// import { URL } from "../../../urls";
 
 export default function FeaturesList() {
   const [colleges, setColleges] = useState([]);
@@ -13,7 +13,7 @@ export default function FeaturesList() {
   useEffect(() => {
     async function fetchColleges() {
       try {
-        const response = await axios.get(`${URL}/college`);
+        const response = await axios.get(`/college`);
         const data = response.data.colleges;
         const formattedColleges = Object.keys(data).map((key) => ({
           title: data[key].name,
