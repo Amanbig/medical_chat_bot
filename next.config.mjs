9e551ef21afea9919fa89c3d0475146bad94b836
Amanpreet Singh <amanpreetsinghjhiwant@outlook.com>
2024-11-30 11:36:25 +0530

    changed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () => {
+        return [
+          {
+            source: "/api/py/:path*",
+            destination:
+              process.env.NODE_ENV === "development"
+                ? "http://127.0.0.1:8000/backend/py/:path*"
+                : "/backend/",
+          },
+        ]
+    },
+};
 
 export default nextConfig;
 // export default {
